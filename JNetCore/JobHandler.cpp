fa026dd0454f4a93035d8b75f461ba985de49b3a
JinHur <jinh2352@gmail.com>
2024-11-30 19:38:45 +0900

    JobHandler::executablePush 결함 예측 및 수정 (ChattingServer_MultiJob 테스트 필요)

--- JNetCore/JobHandler.cpp
+++ JNetCore/JobHandler.cpp
@@ -2,9 +2,16 @@
 
 void JobHandler::executablePush(JobRef job)
 {
-	const auto ret = InterlockedIncrement(&m_AtomicJobCnt);
+	// Push
+	//const auto ret = InterlockedIncrement(&m_AtomicJobCnt);
+	//m_AllocatedJobs.Enqueue(job);
+	//if (ret > 1)	return;
+	// => JobCnt를 증가하지만 Job Enqueue 전 다른 스레드가 Dequeue를 시도하면,
+	//	  Dequeue에 실패하여 어떠한 스레드도 Job을 수행하지 못하는 현상 발생 가능
+
+	// Push
 	m_AllocatedJobs.Enqueue(job);
-	if (ret > 1)	return;
+	if (InterlockedIncrement(&m_AtomicJobCnt) > 1)	return;
 
 	// Execute
 	while (true) {
